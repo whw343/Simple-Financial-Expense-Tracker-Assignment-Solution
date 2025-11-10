51c8d073e10b966cba55b79790ff272d1c205a2e
Hanyu Man <1345239148@qq.com>
2025-11-10 10:39:12 +0800

    Create project module descriptor file

---
+++ module-info.java
@@ -0,0 +1,8 @@
+/**
+ * PROG2004 A2 Theme Park Management System Module
+ * Defines the module name and marks it as an open module (allows reflective access, simplifying IO/reflective operations in the assignment)
+ */
+open module prog2004.a2.prvms {
+    // Declares dependency on the Java base module (implicitly dependent by default, can be explicitly written for readability)
+    requires java.base;
+}
